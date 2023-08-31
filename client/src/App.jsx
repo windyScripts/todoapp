3803e95edd0698b3b78d8d18eb8cd30fd0d70729
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-09-01 01:46:16 +0530

    completes basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,52 @@
-import './App.css'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './components/Layout';
+import TodoForm from './components/ToDoForm';
+import TodoList from './components/ToDoList';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleCharactersError = (value) => {
+    if (value.length < 3 || value.length > 50) {
+      throw new Error(
+        alert(
+          'Todo must have at least 3 characters and less than 50 characters.'
+        )
+      );
+    }
+  };
+
+  const addTodo = async () => {};
+
+  const getAllTodos = async () => {};
+
+  const updateTodo = async (id) => {};
+
+  const deleteTodo = async (id) => {};
+
+  const handleSubmit = (event) => {};
+
+  useEffect(() => {
+    getAllTodos();
+  }, [todoList]);
 
   return (
-    <>
-    </>
-  )
+    <div className='App'>
+      <Layout>
+        <TodoForm handleSubmit={handleSubmit} setTodo={setTodo} todo={todo} />
+        <TodoList
+          todoList={todoList}
+          setNewTodo={setNewTodo}
+          updateTodo={updateTodo}
+          deleteTodo={deleteTodo}
+        />
+      </Layout>
+    </div>
+  );
 }
 
-export default App
+export default App;
